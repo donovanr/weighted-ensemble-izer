da03e4a659ee66775e4ab8284a92b02ac5c22602
Rory Donovan <donovanr@alumni.reed.edu>
2015-07-01 21:41:54 -0400

    added gitignore file

---
+++ westpa_example/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf example_system_cube
+rm -rf istates 
+rm -rf seg_logs
+rm -rf traj_segs
+
+rm -f system.pyc
+rm -f west.h5
+rm -f west.log
+
---
+++ westpa_example/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source env.sh
+ps aux | grep w_run | grep -v grep
+pkill -9 -f w_run
+
+SFX=.d$$
+mv traj_segs{,$SFX}
+mv seg_logs{,$SFX}
+rm -Rf traj_segs$SFX seg_logs$SFX istates$SFX & disown %1
+rm -f system.h5 west.h5 seg_logs.tar
+mkdir seg_logs traj_segs istates
+
+
+echo "running on node: $HOSTNAME" || exit 1
+echo "shell is $SHELL" || exit 1
+
+export WEST_SIM_ROOT="$PWD"
+
+export PYTHONPATH=.
+
+cd $WEST_SIM_ROOT
+SIM_FILES=$(eval ls bstates)
+
+cp -r bstates/* istates/
+cp -r bstates/* $WEST_SIM_ROOT
+
+BSTATE_ARGS="--bstate=well1,1,${MODEL_NAME}"
+$WEST_ROOT/bin/w_init $BSTATE_ARGS --work-manager=threads --n-workers=8 "$@"
+
