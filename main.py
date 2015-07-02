780a0062a520ccd3cf407c02b8cb8869df5c3c60
Rory Donovan <donovanr@alumni.reed.edu>
2015-07-01 22:04:55 -0400

    made template initialization into a funciton in main

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+# python script to take an mcell simulation that was exported from cellblender
+# and output a ready-to-go WESTPA simulation, according to a single parameter file
+
+
 import yaml
 import read_config
 
 import os, subprocess, shutil, tempfile
 
+template_directory = 'westpa_template'
+output_directory = 'output_simulation'
 
-mypath = 'a_new_dir'
-try:
-    os.makedirs(mypath)
-except OSError:
-    if not os.path.isdir(mypath):
-        raise
+def initialize_tempelate(template_dir,output_dir):
+    # delete previous copy of output_simulationr
+    try:
+       shutil.rmtree(output_dir)
+    except OSError:
+       if not os.path.isdir(output_dir):
+           pass
+
+    # make new copy of output_simulation
+    try:
+        shutil.copytree(template_dir,output_dir)
+    except OSError:
+        if os.path.isdir(output_dir):
+            pass
+
+initialize_tempelate(template_directory,output_directory)
