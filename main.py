ed473779570fdcae1ddce3de123e5d8df6439e39
Rory Donovan <donovanr@alumni.reed.edu>
2015-07-01 21:37:19 -0400

    added directory cretion code to main

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import yaml
 import read_config
 
-import os, shutil
+import os, subprocess, shutil, tempfile
+
+
+mypath = 'a_new_dir'
+try:
+    os.makedirs(mypath)
+except OSError:
+    if not os.path.isdir(mypath):
+        raise
--- test.py
+++ test.py
@@ -1,8 +1,23 @@
+import yaml
+import read_config
+
+import os, subprocess, shutil, tempfile
+
 #test the import config function
 my_configs = read_config.import_config("config.yaml")
-for item in my_configs.items():
-   print item
+#for item in my_configs.items():
+#   print item
 
 # test writing out a yaml file
-with open('out_data.yml', 'w') as outfile:
+#with open('out_data.yml', 'w') as outfile:
+#   outfile.write( yaml.dump(my_configs,default_flow_style=False) )
+
+
+
+dirpath = tempfile.mkdtemp()
+with open(dirpath + '/out_data.yml', 'w') as outfile:
    outfile.write( yaml.dump(my_configs,default_flow_style=False) )
+
+my_cmd = 'cat {0}/out_data.yml'.format(dirpath)
+os.system(my_cmd)
+shutil.rmtree('/path/to/folder')
